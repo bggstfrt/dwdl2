c83389baa05c089f40bdb6d031052753f9fa3d5d
bggstfrt <sparky@LSs-Mac-mini.local>
2016-03-23 10:40:09 +0200

    added fastclick

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,12 @@
 // Ionic Starter App
 
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
+
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
